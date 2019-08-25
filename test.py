2f841372242d210aa4fc1fc74f21a6f5fbfb884a
fuckoff cunt <fuckoff@cunt.com>
2019-08-25 20:13:02 +0100

    lol

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import src.crawler as crawler 
 import src.scanner as scanner
+import sys
 
 crawler = crawler.Crawler()
-crawler.setoptions(depth=5, prx="127.0.0.1:8888")
+crawler.setoptions(depth=int(sys.argv[2]))
 
-url = "http://energyservicesme.com"
-crawled = crawler.crawl(url, prx="127.0.0.1:8888")
+url = sys.argv[1]
+crawled = crawler.crawl(url)
 print(crawled)
 
-vulns, nonvuln = scanner.scan(crawled, prx="127.0.0.1:8888")
-print("VULNS: [%s] " % (vulns))
-print("Non-Vulns: [%s] " % (nonvuln))
-
+vulns = scanner.scan(crawled)
+print("VULNS: %s " % (vulns))
